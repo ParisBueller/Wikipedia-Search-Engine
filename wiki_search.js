a1d835e71c697b8efa72d6383cae49d283606d86
Paris Ward <wardparis.pw@gmail.com>
2018-03-06 21:45:28 -0800

    Added if/else statement and ajax call for wikipedia api

--- wiki_search.js
+++ wiki_search.js
@@ -1,5 +1,27 @@
+//Global variables
+var api = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=";
+var formData;
+
+//if our input is empty, retrieve a random wikipedia page
 $("#search").click(function(){
-    if($("#search").val()== ""){
-      window.open("https://en.wikipedia.org/wiki/Special:Random")
-    }
-  })
+  if($("#search").val()== ""){
+    window.open("https://en.wikipedia.org/wiki/Special:Random")
+  }
+ //otherwise, search wikipedia for what was typed in our input form 
+  else{
+    function searchWikipedia(){
+      formData = document.getElementById('searchForm').value;
+      var wikiApi = api + formData;
+      console.log(wikiApi);
+      $.ajax({
+        url: wikiApi, success: function(response){
+          
+
+}
+        
+
+
+      })
+    }    
+  }
+})
