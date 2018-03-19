58b7b4c6120eb52d9abed45d4bf26a4a702c65f0
Paris Ward <wardparis.pw@gmail.com>
2018-03-18 18:11:40 -0700

    A working wikipedia search app

--- wiki_search.js
+++ wiki_search.js
@@ -1,21 +1,33 @@
-var api = "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
-var input;
+$(document).ready(function(){
+  $("#search").click(function(){ 
+  var searchTerm = $('#searchTerm').val();
+  var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?"
+ //if searchTerm is empty, open a new page with a random wiki    
+    if(searchTerm == '') {
+      window.open("https://en.wikipedia.org/wiki/Special:Random");
+    }
 
-//Search Bar Component
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = { term: ''};
-}  
-  render() {
-    
-    return (
-      <div className="search-bar">
-        <input value={this.state.term} onChange={event => this.onInputChange(event.target.value)} /> 
-      </div>
-    ); 
-  }
-}
-
-ReactDOM.render(<SearchBar />, document.getElementById('search'));
+    $.ajax({
+      type:'GET',
+      url: api,
+      async:false,
+      dataType:'json',
+      success: (data) => {
+       //Get Heading console.log(data[1][0]);
+       //Get Description console.log(data[2][0]);
+       //Get Link URL console.log(data[3][0]);
+        //Clear prior search results
+      $("#searchResults").html(''); 
+        //Iterate through our results and prepend them to a list
+        for(var i =0; i < data[1].length; i++) {        
+          $("#searchResults").prepend("<li><a href="+data[3][i]+">"+data[1][i] +"</a><p>"+data[2][i]+"</p></li>");
+        }
+        //Empty out search bar once something has been searched for
+        $("#searchTerm").val('');
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  });
+});
