a95b2adab509a21b79213feb247d6bea8b4a479d
Paris Ward <wardparis.pw@gmail.com>
2018-08-22 11:43:08 -0700

    updated cdns,changed to es6 syntax, added preventDefault on submit and adjusted search form

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-$(document).ready(function(){
-  $("#search").click(function(){ 
-  var searchTerm = $('#searchTerm').val();
-  var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?"
+
+$(document).ready(() => {
+  $("form").on('submit', (e) => {
+    e.preventDefault();
+    $("#search").click();
+  });
+  
+  
+  $("#search").click(() => { 
+  let searchTerm = $('#searchTerm').val();
+  let api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?"
  //if searchTerm is empty, open a new page with a random wiki    
     if(searchTerm == '') {
       window.open("https://en.wikipedia.org/wiki/Special:Random");
@@ -13,13 +20,16 @@ $(document).ready(function(){
       async:false,
       dataType:'json',
       success: (data) => {
-       //Get Heading console.log(data[1][0]);
-       //Get Description console.log(data[2][0]);
-       //Get Link URL console.log(data[3][0]);
+       //Get Heading 
+        console.log(data[1][0]);
+       //Get Description 
+        console.log(data[2][0]);
+       //Get Link URL 
+        console.log(data[3][0]);
         //Clear prior search results
       $("#searchResults").html(''); 
         //Iterate through our results and prepend them to a list
-        for(var i =0; i < data[1].length; i++) {        
+        for(let i =0; i < data[1].length; i++) {        
           $("#searchResults").prepend("<li><a href="+data[3][i]+">"+data[1][i] +"</a><p>"+data[2][i]+"</p></li>");
         }
         //Empty out search bar once something has been searched for
